001e6cba95e42dbbddb480a0aa16852d6bddb094
Tasnimul Hasan Mahi <mahitasnimul2@gmail.com>
2026-07-04 19:33:10 +0600

    update car details

--- src/app/components/RelatedCars.js
+++ src/app/components/RelatedCars.js
@@ -2,14 +2,14 @@ import Car from "./Car";
 
 export default function RelatedCars({ data }) {
   return (
-    <div className="flex flex-wrap -mx-4">
-      <div className="w-full px-4 wow fadeInUp mt-14">
+    <div className="flex flex-wrap ">
+      <div className="w-full  wow fadeInUp mt-14">
         <h2 className="relative pb-5 text-2xl font-semibold text-dark  sm:text-[36px]">
           Other Vehicles You May Like
         </h2>
         <span className="mb-10 inline-block h-[2px] w-20 bg-primary"></span>
       </div>
-      <div className="grid lg:grid-cols-4 sm:grid-cols-1">
+      <div className="grid lg:grid-cols-4 sm:grid-cols-1 gap-3">
         {data?.map((car) => (
           <Car key={car.id} car={car} />
         ))}
